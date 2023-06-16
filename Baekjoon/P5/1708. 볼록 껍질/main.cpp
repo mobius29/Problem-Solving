e5d2a9b102b5a2043bca3815f68ce74b68220c60
mobius29 <sagjin0000@naver.com>
2023-06-16 17:10:21 +0900

    1708 complete

--- Baekjoon/P5/1708. 볼록 껍질/main.cpp
+++ Baekjoon/P5/1708. 볼록 껍질/main.cpp
@@ -10,16 +10,16 @@
 using namespace std;
 
 typedef long long ll;
-typedef pair<int ,int> pii;
+typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
 typedef vector<int>  vi;
 typedef vector<ll> vl;
 typedef vector<bool> vb;
-typedef vector<pii> vii;
+typedef vector<pll> vii;
 typedef vector<pll> vll;
 typedef tuple<int, int, int> ti;
 typedef tuple<ll, ll, ll> tl;
-typedef pii point;
+typedef pll point;
 
 const int INF = 0x3f3f3f3f;
 const ll LINF = 0x3f3f3f3f3f3f3f3f;
@@ -28,110 +28,81 @@ const int dx[] = {-1, 0, 1, 0};
 const int dy[] = {0, -1, 0, 1};
 
 int N;
-vii point_list, answer_list;
+pll min_y_point;
+vll point_list;
 
-double get_distance(pii, pii, pii);
-int find_max_distance_idx(vii, pii, pii, pii);
-void solve(vii, pii, pii);
+ll get_ccw(pll, pll, pll);
+ll get_distance(pll, pll);
+bool compare(pll, pll);
+stack<pll> solve();
 
 int main() {
   sync(); cin >> N;
   for(int i = 0; i < N; ++i) {
-    int x, y; cin >> x >> y;
+    ll x, y; cin >> x >> y;
     point_list.pb({ x, y });
   }
 
-  sort(all(point_list));
+  min_y_point = point_list[0];
+  for (int i = 1; i < N; ++i) {
+    pll item = point_list[i];
+    if (item.second > min_y_point.second) continue;
 
-  vii left, right;
-  for(int i = 0; i < N; ++i) {
-    double dist = get_distance(point_list[0], point_list[N-1], point_list[i]);
-    if (dist == 0) continue;
-
-    if (dist > 0) right.pb(point_list[i]);
-    else left.pb(point_list[i]);
-  }
-
-  if (left.size() > 0) solve(left, point_list[N-1], point_list[0]);
-  if (right.size() > 0) solve(right, point_list[0], point_list[N-1]);
-
-  answer_list.pb(point_list[0]); answer_list.pb(point_list[N-1]);
-  cout << answer_list.size() << endl;
-}
-
-double get_distance(pii start, pii end, pii p) {
-  ll x2_x1 = end.first - start.first, y2_y1 = end.second - start.second;
-  ll y1_y0 = start.second - p.second, x1_x0 = start.first - p.first;
-
-  ll denomi_x = x2_x1 * x2_x1, denomi_y = y2_y1 * y2_y1;
-  ll denomi = denomi_x + denomi_y;
-
-  ll numera_x = x2_x1 * y1_y0, numera_y = x1_x0 * y2_y1;
-  ll numera = (numera_x - numera_y) * (numera_x - numera_y);
-
-  return (double)numera / denomi;
-}
-
-int find_max_distance_idx(vii list, pii start, pii end) {
-  int max_distance_idx = 0;
-  double max_distance = 0;
-  for (int i = 1; i < list.size(); ++i) {
-    double dist = get_distance(start, end, list[i]);
-    if (dist == 0) continue;
-
-    if (dist > max_distance) {
-      max_distance_idx = i;
-      max_distance = dist;
+    if (item.second < min_y_point.second || item.first < min_y_point.first) {
+      min_y_point = item;
     }
   }
 
-  return max_distance_idx;
+  sort(all(point_list), compare);
+  
+  stack<pll> answer = solve();
+
+  cout << answer.size() << endl;
 }
 
-void solve(vii list, pii start, pii end) {
-  int max_distance_idx = find_max_distance_idx(list, start, end);
-  pii max_distance_point = list[max_distance_idx]; answer_list.pb(max_distance_point);
-  bool is_left_to_right = end.first - start.first > 0;
+ll get_ccw(pll a, pll b, pll c) {
+  return (b.first - a.first) * (c.second - a.second) - (c.first - a.first) * (b.second - a.second);
+}
 
-  vii start_to_max_idx_list, max_idx_to_end_list;
-  for (int i = 0; i < list.size(); ++i) {
-    if (i == max_distance_idx) continue;
+ll get_distance(pll a, pll b) {
+  ll x = b.first - a.first;
+  ll y = b.second - a.second;
 
-    if (is_left_to_right) {
-      if (i < max_distance_idx) {
-        double distance = get_distance(start, max_distance_point, list[i]);
-        if (distance > 0) start_to_max_idx_list.pb(list[i]);
-      } else {
-        double distance = get_distance(max_distance_point, end, list[i]);
-        if (distance > 0) max_idx_to_end_list.pb(list[i]);
+  return x * x + y * y;
+}
+
+bool compare(pll a, pll b) {
+  ll ccw = get_ccw(min_y_point, a, b);
+
+  if (ccw == 0) {
+    return get_distance(min_y_point, a) < get_distance(min_y_point, b);
+  }
+
+  return ccw > 0;
+}
+
+stack<pll> solve() {
+  stack<pll> st;
+  st.push(point_list[0]); st.push(point_list[1]);
+
+  for (int i = 2; i < N; ++i) {
+    st.push(point_list[i]);
+    while (st.size() > 2) {
+      pll c = st.top(); st.pop();
+      pll b = st.top(); st.pop();
+      pll a = st.top();
+
+      ll ccw = get_ccw(a, b, c);
+
+      if (ccw > 0) {
+        st.push(b); st.push(c);
+        break;
       }
-    }
 
-    else {
-      if (i < max_distance_idx) {
-        double distance = get_distance(max_distance_point, end, list[i]);
-        if (distance > 0) max_idx_to_end_list.pb(list[i]);
-      } else {
-        double distance = get_distance(start, max_distance_point, list[i]);
-        if (distance > 0) start_to_max_idx_list.pb(list[i]); 
-      }
+      st.push(c);
+      if (ccw == 0) break;
     }
   }
 
-  if (start_to_max_idx_list.size() > 0) solve(start_to_max_idx_list, start, max_distance_point);
-  if (max_idx_to_end_list.size() > 0) solve(max_idx_to_end_list, max_distance_point, end);
+  return st;
 }
-
-/*
-8
-1 1
-1 2
-1 3
-2 1
-2 2
-2 3
-3 1
-3 2
-
-5
-*/
