f9fcc1ce956461d4ece98f9c8f9d12faa5ff3644
mobius29 <sagjin0000@naver.com>
2023-06-19 17:12:12 +0900

    feat: 27449 하는 중

--- Baekjoon/P4/27449. 한별이 드롭킥/main.cpp
+++ Baekjoon/P4/27449. 한별이 드롭킥/main.cpp
@@ -29,12 +29,14 @@ const int dx[] = {-1, 0, 1, 0};
 const int dy[] = {0, -1, 0, 1};
 
 int N, M;
-vii updrafts_pos, updrafts_neg;
+vll updrafts_pos, updrafts_neg;
 
 int main() {
   sync(); cin >> N >> M;
+
+  updrafts_pos.pb({ 0, 0 }); updrafts_neg.pb({ 0, 0 });
   for(int i = 0; i < N; ++i) {
-    int x, p; cin >> x >> p;
+    ll x, p; cin >> x >> p;
 
     if (x < 0) updrafts_neg.pb({ -x, p });
     else updrafts_pos.pb({ x, p });
@@ -42,4 +44,36 @@ int main() {
 
   sort(all(updrafts_neg));
   sort(all(updrafts_pos));
+
+  vector<vll> dp[2];
+  for (int i = 0; i < 2; ++i)
+    dp[i].assign(updrafts_pos.size() + 1, vll(updrafts_neg.size() + 1, { -LINF, -LINF }));
+
+  dp[0][0][0] = dp[1][0][0] = { M, M };
+  for (int i = 1; i <= updrafts_pos.size(); ++i) {
+    auto [xi, pi] = updrafts_pos[i];
+    auto [max_height, cur_height] = dp[0][i-1][0];
+
+    ll distance = xi - updrafts_pos[i-1].first;
+    if (cur_height < distance) break;
+
+    ll next_height = cur_height == distance ? 0 : cur_height - distance + pi;
+    dp[0][i][0] = { max_height + pi, next_height };
+
+    if (next_height == 0) break;
+  }
+
+  for (int j = 1; j <= updrafts_pos.size(); ++j) {
+    auto [xi, pi] = updrafts_neg[j];
+    auto [max_height, cur_height] = dp[0][0][j-1];
+
+    ll distance = xi - updrafts_neg[j-1].first;
+    if (cur_height < distance) break;
+
+    ll next_height = cur_height == distance ? 0 : cur_height - distance + pi;
+    dp[1][0][j] = { max_height + pi, next_height };
+
+    if (next_height == 0) break;
+  }
+
 }
