1538fd9480b321a9df47f7683fe317ee4075c193
mobius29 <sagjin0000@naver.com>
2023-06-28 16:12:54 +0900

    feat: solving

--- Baekjoon/P4/2188. 축사 배정/main.cpp
+++ Baekjoon/P4/2188. 축사 배정/main.cpp
@@ -3,7 +3,7 @@
 #define endl "\n"
 #define ends " "
 #define pb push_back
-#define MAX 200
+#define MAX 201
 using namespace std;
 
 typedef pair<int ,int> pii;
@@ -13,17 +13,33 @@ typedef vector<bool> vb;
 const int INF = 0x3f3f3f3f;
 
 int N, M, ans;
+int matched_stable[MAX];
 vi wanted_stable_list[MAX];
+
+void solve(int current) {
+  if (current > N) return;
+
+  int start = current;
+  for (auto stable: wanted_stable_list[current]) {
+    while(true) {
+      int stabled_cow = matched_stable[start];
+      if (stabled_cow == 0) {
+      }
+    }
+  }
+}
 
 int main() {
   sync(); cin >> N >> M;
-  for (int i = 0; i < N; ++i) {
+  for (int i = 1; i <= N; ++i) {
     int S; cin >> S;
 
     wanted_stable_list[i].assign(S, 0);
-    for (int j = 0; j < S; ++j)
+    for (int j = 1; j <= S; ++j)
       cin >> wanted_stable_list[i][j];
   }
 
-  
+  for (int i = 1; i <= N; ++i) {
+    solve(i);
+  }
 }
